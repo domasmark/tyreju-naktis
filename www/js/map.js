070ced5194269a141e9eac117eb68aab9e87d619
Domas Markevičius <dmarkevicius@qbcode.com>
2013-08-13 17:20:19 +0300

    improved toggle effect

--- www/js/map.js
+++ www/js/map.js
@@ -13,60 +13,56 @@ var onSuccess = function(position) {
 	//	  'Heading: '           + position.coords.heading           + '\n' +
 	//	  'Speed: '             + position.coords.speed             + '\n' +
 	//	  'Timestamp: '         + position.timestamp                + '\n');
-		  var mapOptions = {
-		  center : new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-		  zoom: 14,
-		  mapTypeId: google.maps.MapTypeId.ROADMAP
-		  };
-		  var map = new google.maps.Map(document.getElementById("mapcontent"),
-			mapOptions);
-		var marker = new google.maps.Marker({
-			position: mapOptions.center,
-			map: map,
-			title:"Your position",
-			clickable: true,
-			icon: { path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW, scale:6 }
-			
-		});
-		google.maps.event.addListener(marker, 'click', function (){
-			window.location.replace('http://www.google.com');
-		
-		
-		});
-		map.set('styles', [
-			{
-				featureType: 'road',
-				elementType: 'geometry',
-				stylers: [
-					{ color: '#ffffff' }, {weight: 1.0}
-				]
-			}, {
-				featureType: 'road',
-				elementType: 'labels',
-				stylers: [
-					{ color: '#000055' }, {weight: 0.2}
-				]
-			}, {
-				featureType: 'poi',
-				elementType: 'labels',
-				stylers: [
-					{ visibility: 'off' }
-				]
-			}, {
-				featureType: 'road.highway',
-				elementType: 'labels',
-				stylers: [
-					{ visibility: 'off' }
-				]
-			}, {
-				featureType: 'poi',
-				elementType: 'geometry',
-				stylers: [
-					{ visibility: 'off' }
-				]
-			}
-			
-			]);
+	var mapOptions = {
+		center : new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+		zoom: 14,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("mapcontent"),mapOptions);
+	var marker = new google.maps.Marker({
+		position: mapOptions.center,
+		map: map,
+		title:"Your position",
+		clickable: true,
+		icon: { path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW, scale:6 }
+	});
+	google.maps.event.addListener(marker, 'click', function (){
+		window.location.replace('http://www.google.com');
+	});
+	map.set('styles', [
+		{
+			featureType: 'road',
+			elementType: 'geometry',
+			stylers: [
+				{ color: '#ffffff' }, {weight: 1.0}
+			]
+		}, {
+			featureType: 'road',
+			elementType: 'labels',
+			stylers: [
+				{ color: '#000055' }, {weight: 0.2}
+			]
+		}, {
+			featureType: 'poi',
+			elementType: 'labels',
+			stylers: [
+				{ visibility: 'off' }
+			]
+		}, {
+			featureType: 'road.highway',
+			elementType: 'labels',
+			stylers: [
+				{ visibility: 'off' }
+			]
+		}, {
+			featureType: 'poi',
+			elementType: 'geometry',
+			stylers: [
+				{ visibility: 'off' }
+			]
+		}
+	
+	]);
 };
 
 // onError Callback receives a PositionError object
--- www/js/toggle.js
+++ www/js/toggle.js
@@ -1,20 +1,18 @@
 var speed = 'fast';
 $(document).ready(function() {
 	//onLoad
-	//List Menu is off-screen
-	$("#top").animate({left:"200%"},speed);
-	$("#content").animate({left:"-200%"},speed);
-	$("#mapcontent").animate({left:"-200%"},speed);
-	$("#gamecontent").animate({left:"-200%"},speed);
-	//Main Menu is visible
-	$("#bottom").animate({left:"0"},speed);
-	$("#logo").animate({left:"50%"},speed);
+	$('#content').hide();
+	$('#mapcontent').hide();
+	$('#gamecontent').hide();
+	$('#top').hide();
 	//Lists
 	$('#citylist').show();
 	$('#unilist').hide();
 
 	//Main Menu Buttons
 	$(".button.list").click(function(){
+		$('#top').show();
+		$('#content').show();
 		$("#bottom").animate({left:"-200%"},speed);
 		$("#logo").animate({left:"-200%"},speed);
 		$("#toptitle").html("Programa");
@@ -23,6 +21,8 @@ $(document).ready(function() {
 		$("#content").animate({left:"0"},speed);
 	});
 	$(".button.map").click(function(){
+		$('#top').show();
+		$('#mapcontent').show();
 		$("#bottom").animate({left:"-200%"},speed);
 		$("#logo").animate({left:"-200%"},speed);
 		$("#toptitle").html("Žemėlapis");
@@ -31,6 +31,8 @@ $(document).ready(function() {
 		$("#mapcontent").animate({left:"0"},speed);
 	});
 	$(".button.game").click(function(){
+		$('#top').show();
+		$('#gamecontent').show();
 		$("#bottom").animate({left:"-200%"},speed);
 		$("#logo").animate({left:"-200%"},speed);
 		$("#toptitle").html("Žaidimas");
@@ -41,10 +43,14 @@ $(document).ready(function() {
 
 	//List Menu back button
 	$("#back").click(function(){
+		$('#top').hide();
+		$('#content').hide();
+		$('#mapcontent').hide();
+		$('#gamecontent').hide();
 		$("#top").animate({left:"200%"},speed);
-		$("#content").animate({left:"-200%"},speed);
-		$("#mapcontent").animate({left:"-200%"},speed);
-		$("#gamecontent").animate({left:"-200%"},speed);
+		$("#content").animate({left:"200%"},speed);
+		$("#mapcontent").animate({left:"200%"},speed);
+		$("#gamecontent").animate({left:"200%"},speed);
 
 		$("#bottom").animate({left:"0"},speed);
 		$("#logo").animate({left:"50%"},speed);
